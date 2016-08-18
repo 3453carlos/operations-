7600505df3395380773fb5d3a6d0a3371fbee366
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-18 00:14:56 -0500

    Regresa 0 en caso de arreglo vacío

--- promedio-de-impares.java
+++ promedio-de-impares.java
@@ -1,4 +1,5 @@
     public static float promedioDeImpares(int[] datos){
+        if(datos.length <= 0) return 0;
         int a = 0;
         for(int numero:datos) if(numero % 2 != 0) a += numero;
         return a / datos.length;
