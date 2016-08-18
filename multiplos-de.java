0e7c8e4d8d26aff6be0f80368863bc9edb17cfd4
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-18 00:14:04 -0500

    Regresa 0 en caso de arreglo vacío

--- multiplos-de.java
+++ multiplos-de.java
@@ -1,4 +1,5 @@
     public static String multiplosDe(int[] datos, int multiplo){
+        if(datos.length <= 0) return 0;
         String r = "";
         for(int n:datos) if(n % multiplo == 0) r += String.valueOf(n) + "\n";
         return r;
