7781204220970465b13baf9e2c5e31e700b0af70
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-18 00:15:10 -0500

    Regresa 0 en caso de arreglo vacío

--- suma-de-pares.java
+++ suma-de-pares.java
@@ -1,4 +1,5 @@
     public static int sumaDePares(int[] datos){
+        if(datos.length <= 0) return 0;
         int a = 0;
         for(int numero:datos) if(numero % 2 == 0) a += numero;
         return a;
