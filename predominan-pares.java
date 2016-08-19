f7ff9269fe5c6a400303de37df8df68762a0dcef
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-19 01:44:05 -0500

    Optimizaciones

--- predominan-pares.java
+++ predominan-pares.java
@@ -1,5 +1,5 @@
     public static boolean predominanPares(int[] datos){
-        if(datos.length <= 0) return 0;
+        if(datos.length <= 0) return false;
         int contadorPares = 0,
             contadorImpares = 0;
         for(int numero: datos) if(numero % 2 == 0) contadorPares++; else contadorImpares++;
