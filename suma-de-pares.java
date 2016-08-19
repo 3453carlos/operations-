9a277dd8f542236d79b5069ea5940ce06193858b
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-19 01:46:48 -0500

    Optimizaciones

--- suma-de-pares.java
+++ suma-de-pares.java
@@ -1,6 +1,6 @@
-    public static int sumaDePares(int[] datos){
-        if(datos.length <= 0) return 0;
+    public static int sumaDePares(int[] d){
+        if(d.length <= 0) return 0;
         int a = 0;
-        for(int numero:datos) if(numero % 2 == 0) a += numero;
+        for(int n:d) if(n % 2 == 0) a += n;
         return a;
     }
