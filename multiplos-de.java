e9ad7db665c4c45bf630e654fb20a83a9a296a23
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-19 01:43:27 -0500

    Optimizaciones

--- multiplos-de.java
+++ multiplos-de.java
@@ -1,6 +1,6 @@
-    public static String multiplosDe(int[] datos, int multiplo){
-        if(datos.length <= 0) return 0;
+    public static String multiplosDe(int[] d, int m){
+        if(d.length <= 0 || m <= 0) return "No válido";
         String r = "";
-        for(int n:datos) if(n % multiplo == 0) r += String.valueOf(n) + "\n";
+        for(int n:d) if(n % m == 0) r += String.valueOf(n) + "\n";
         return r;
     }
