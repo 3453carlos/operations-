881535f4e6696ea9211cb5d7342beb2c80a99141
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-19 01:43:48 -0500

    Optimizaciones

--- predominan-impares.java
+++ predominan-impares.java
@@ -1,4 +1,4 @@
     public static boolean predominanImpares(int[] datos){
-        if(datos.length <= 0) return 0;
+        if(datos.length <= 0) return false;
         return !predominanPares(datos);
     }
