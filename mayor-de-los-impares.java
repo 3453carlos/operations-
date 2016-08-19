66a5c36a4519a9fda6e7d1d36cd7eed0004041e5
Luis Cabrera Benito <luiscabrerabenito970621@gmail.com>
2016-08-19 01:39:02 -0500

    Optimizaciones

--- mayor-de-los-impares.java
+++ mayor-de-los-impares.java
@@ -1,6 +1,6 @@
-    public static int mayorDeLosImpares(int[] datos){
-        if(datos.length <= 0) return 0;
-        int mayor = datos[0];
-        for(int numero: datos) if(numero >= mayor && numero % 2 != 0) mayor = numero;
-        return mayor;
-    } 
+    public static int mayorDeLosImpares(int[] d){
+        if(d.length <= 0) return 0;
+        int m = d[0];
+        for(int x = d.length - 1; x >= 1; x--) if(d[x] >= m && d[x] % 2 != 0) m = d[x];
+        return m;
+    }
